9f607d6b0b8a40cb5772fe484b4abdd7cae23598
mwaskom <mwaskom@stanford.edu>
2017-04-27 14:43:55 -0400

    Tweak stimulus parameters

--- params.py
+++ params.py
@@ -4,21 +4,21 @@ base = dict(
     display_name="laptop",
     display_luminance=0,
 
-    field_size=18,
+    field_size=24,
 
-    bar_width=1,
-    bar_segment_gap=.5,
+    bar_width=1.5,
+    bar_segment_gap=1,
 
     key_names=["left", "down", "right"],
 
-    traversal_duration=18,
-    traversal_steps=12,
+    traversal_duration=24,
+    traversal_steps=18,
 
     monitor_eye=True,
 
-    dot_size=.05,
+    dot_size=.1,
     dot_color=1,
-    dot_density=30,
+    dot_density=50,
     dot_speed=5,
     dot_interval=3,
 
