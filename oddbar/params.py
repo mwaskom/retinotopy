d93b44e18aa5e97df2b8b23a38b5fe869347f695
mwaskom <mwaskom@stanford.edu>
2017-10-13 15:09:26 -0400

    Formatting

--- oddbar/params.py
+++ oddbar/params.py
@@ -1,23 +1,24 @@
 base = dict(
 
-display_name="laptop",
+    display_name="macbook-air",
+    display_luminance=35,
 
-fix_radius=.1,
+    fix_radius=.1,
 
-field_size=16,
-step_duration=1.5,
+    field_size=16,
+    step_duration=1.5,
 
-bar_width=1.5,
+    bar_width=1.5,
 
-element_size=2,
-element_tex="saw",
-element_sf=1.5,
+    element_size=2,
+    element_tex="saw",
+    element_sf=1.5,
 
-update_rate=2,
+    update_rate=2,
 
-oddball_sf=2,
-oddball_prob=.1,
+    oddball_sf=2,
+    oddball_prob=.1,
 
-drift_rate=.5,
+    drift_rate=.5,
 
 )
